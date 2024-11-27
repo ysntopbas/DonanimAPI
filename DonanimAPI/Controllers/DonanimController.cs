2ee1045ba875a567c1b4e16a9a77184f4b901408
Yasin <Yasin@YASIN-PC>
2024-11-27 16:02:32 +0300

    upsert eklendi

--- DonanimAPI/Controllers/DonanimController.cs
+++ DonanimAPI/Controllers/DonanimController.cs
@@ -28,7 +28,23 @@ namespace DonanimAPI.Controllers
 
             try
             {
-                await _donanimBilgileriCollection.InsertOneAsync(donanimBilgileri);
+                // Veritabanında DeviceID'ye göre kontrol et
+                var filter = Builders<DonanimBilgileri>.Filter.Eq(d => d.DeviceID, donanimBilgileri.DeviceID);
+
+                // Güncelleme için Update işlemi
+                var update = Builders<DonanimBilgileri>.Update
+                    .Set(d => d.CpuInfos, donanimBilgileri.CpuInfos)
+                    .Set(d => d.GpuInfos, donanimBilgileri.GpuInfos)
+                    .Set(d => d.RamInfo, donanimBilgileri.RamInfo)
+                    .Set(d => d.DiskInfo, donanimBilgileri.DiskInfo)
+                    .Set(d => d.Date, donanimBilgileri.Date)
+                    .Set(d => d.DeviceName, donanimBilgileri.DeviceName);
+
+                // Upsert işlemi için UpdateOptions
+                var options = new UpdateOptions { IsUpsert = true };
+
+                // DeviceID'ye göre upsert işlemini yap
+                await _donanimBilgileriCollection.UpdateOneAsync(filter, update, options);
             }
             catch (Exception ex)
             {
