bc588259112c145971b976bc36b19f39f16091ef
Yasin <Yasin@YASIN-PC>
2024-12-28 20:34:32 +0300

    cors settings allowed all sites

--- DonanimAPI/Program.cs
+++ DonanimAPI/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("https://hardwareasyle.netlify.app")
+            .WithOrigins("*")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
